1414a8c852c3173be6506ef7552a2a5ed3a6580a
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-08-06 22:50:41 +0500

    configuration done

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let x = 10;
+if (x < 50)
+    x += 10;
+console.log(x);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let x:number=10
+
+if (x < 50)
+    x += 10;
+console.log(x)
